85c92d1f8b96cb6f2a951113bdcbc92d5f6ef487
Salah-Eddine-Tader <s.tader@harmony-technology.net>
2022-09-22 16:06:15 +0100

    Maj du projet

--- page/page.route.js
+++ page/page.route.js
@@ -1,25 +1,22 @@
-import express from 'express';
+import express from 'express'; 
 import {
-  changeContent,
-  create,
-  update,
-  deletePageRecord,
-  details,
-  list,
-  loadContent,
-} from './page.controller';
+  changeContent, 
+  create, 
+  update, 
+  deletePageRecord, 
+  details, 
+  list, 
+  loadContent, 
+} from './page.controller'; 
 
-const pageRoute = express.Router();
+const pageRoute = express.Router(); 
 
-pageRoute.post('/', create);
-pageRoute.post('/:pageId/content', changeContent);
+pageRoute.post('/',  create); 
+pageRoute.post('/:pageId/content',  changeContent); 
+pageRoute.put('/:pageId',  update); 
+pageRoute.delete('/:pageId',  deletePageRecord); 
+pageRoute.get('/:pageId',  details); 
+pageRoute.get('/',  list); 
+pageRoute.get('/:pageId/content',  loadContent);  
 
-pageRoute.put('/:pageId', update);
-
-pageRoute.delete('/:pageId', deletePageRecord);
-
-pageRoute.get('/:pageId', details);
-pageRoute.get('/', list);
-pageRoute.get('/:pageId/content', loadContent);
-
-export default pageRoute;
+export default pageRoute; 
